5f2d2a85bac267cc0c4aa82cf9f60358de5619eb
Monica Yupa <monica.yupa@gmail.com>
2025-11-12 16:23:12 -0500

    Fix port

--- backend/main.py
+++ backend/main.py
@@ -95,6 +95,9 @@ async def root():
 
 
 if __name__ == "__main__":
+    import os
+
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # fallback to 8000 locally
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
